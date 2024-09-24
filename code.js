3afff190e252b48edca698f1f80693fc21a81f72
abu-jobair-khan <zobair.ahamed9102@gmail.com>
2024-09-24 15:04:46 +0600

    second commit

---
+++ code.js
@@ -0,0 +1,14 @@
+
+let histryButton=document.getElementById('histry');
+let donateButton = document.getElementById('donate');
+function changeColor(){
+   
+   histryButton.style.backgroundColor='green';
+   
+   donateButton.style.backgroundColor='white';
+}
+
+function normalColor(){
+    histryButton.style.backgroundColor='white';
+    donateButton.style.backgroundColor='green';
+}
