2439d058292f443cac0e474812a3d4483d07658a
abu-jobair-khan <zobair.ahamed9102@gmail.com>
2024-09-25 11:28:40 +0600

    fifth

--- code.js
+++ code.js
@@ -1,14 +1,25 @@
-
-let histryButton=document.getElementById('histry');
-let donateButton = document.getElementById('donate');
-function changeColor(){
-   
-   histryButton.style.backgroundColor='green';
-   
-   donateButton.style.backgroundColor='white';
-}
-
-function normalColor(){
-    histryButton.style.backgroundColor='white';
-    donateButton.style.backgroundColor='green';
-}
+document.getElementById('moneyClicked').addEventListener('click', function(){
+    const myAmount = document.getElementById('my-amount').innerText
+    const entrdmoney = document.getElementById('entered-amount').value;
+    const currentBalance = document.getElementById('balance').innerText;
+    let i = 0;
+    for(let x of entrdmoney){
+        if(( 'a' <= x && x <= 'z') || ('A' <= x && x<= 'Z') || x=='' || x=='@'|| x=='&' || x=='#' || x=='$' || x== '%' || x=='*'){
+             i=i+1;
+        }
+        else
+         continue;
+     }
+     
+     if(i == 0){
+        
+        const newBalance = parseFloat(currentBalance) + parseFloat(entrdmoney);
+        document.getElementById('balance').innerText = newBalance;
+        let currentMyAmount = parseFloat(myAmount) - parseFloat(entrdmoney);
+        document.getElementById('my-amount').innerText=currentMyAmount;
+        document.getElementById('entered-amount').value = "";
+     }
+     else{ alert('Invalid Input of money');}
+    
+    
+})
